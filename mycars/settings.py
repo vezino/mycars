aa31bcf2a1203208b082a306307d34020c165dc0
Juan Manuel Rivero <juanmanuelriverom@gmail.com>
2017-01-03 00:37:21 -0600

    add hot fix database

--- mycars/settings.py
+++ mycars/settings.py
@@ -12,6 +12,11 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 
 import os
 
+# Parse database configuration from $DATABASE_URL
+import dj_database_url
+# To transform strings from environment variables to boolean values
+import ast
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
