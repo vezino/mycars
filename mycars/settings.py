e1b4b6b3a0aa7707d47d1793ed4a785b757bd8e8
Juan Manuel Rivero <juanmanuelriverom@gmail.com>
2017-01-03 22:39:48 -0600

    fix white noise

--- mycars/settings.py
+++ mycars/settings.py
@@ -159,7 +159,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+#STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
 STATIC_URL = '/static/'
 #ADMIN_MEDIA_PREFIX = '/static/admin/'
 
