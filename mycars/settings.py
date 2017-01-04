b998c0a27b60cf612f406eae9f43d79b7abd5635
Juan Manuel Rivero <juanmanuelriverom@gmail.com>
2017-01-03 22:16:33 -0600

    remove white noise

--- mycars/settings.py
+++ mycars/settings.py
@@ -158,7 +158,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_URL = '/static/'
 #ADMIN_MEDIA_PREFIX = '/static/admin/'
 
