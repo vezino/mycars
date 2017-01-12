137ff188b8bd7da08a981bd98ee0c5b631129eb2
Juan Manuel Rivero <juanmanuelriverom@gmail.com>
2017-01-12 00:46:57 -0600

    add cover_damage, etc, add validation fro end_at

--- insurances/models.py
+++ insurances/models.py
@@ -5,15 +5,21 @@ from __future__ import unicode_literals
 
 from django.db import models
 from datetime import datetime
+from django.utils import timezone
 from django.utils.encoding import python_2_unicode_compatible
 # Unicode shite
 import unicodedata
 
+# Validation stuff
+from django.core.exceptions import ValidationError
+from django.utils.translation import ugettext_lazy as _
 # Create your models here.
+
 @python_2_unicode_compatible
 class Company(models.Model):
   name = models.CharField("compañia",max_length=100)
-  created_at = models.DateTimeField("fecha de alta",default=datetime.now)
+  created_at = models.DateTimeField("fecha de alta",auto_now_add=True,editable=False)
+  last_modified = models.DateTimeField("ultima modificacion",auto_now=True,editable=False)
   class Meta:
     verbose_name_plural = "Compañias"
 
@@ -22,7 +28,7 @@ class Company(models.Model):
 
 @python_2_unicode_compatible
 class Policy(models.Model):
-  COVER_TYPE = (('0','N/A'),('1','Amplia'),('2','Limitada'),('3','Daños a terceros')
+  COVER_TYPE = (('0','N/A'),('Amplia','Amplia'),('Limitada','Limitada'),('Daños a terceros','Daños a terceros')
     )
   company = models.ForeignKey('Company',
     on_delete=models.CASCADE,
@@ -31,14 +37,42 @@ class Policy(models.Model):
   reference = models.CharField("numero de poliza",max_length=30)
   start_at = models.DateTimeField("inicio de vigencia",default=datetime.now)
   end_at = models.DateTimeField("fin vigencia",default=datetime.now)
-  price = models.FloatField("importe del seguro",default=0,help_text="cuanto costo el seguro")
-  cover_amount = models.FloatField("importe de la coberura",default=0)
-  cover_type = models.CharField("tipo de cobertura",max_length=1,default= 0,choices = COVER_TYPE)
-  created_at = models.DateTimeField("fecha de alta",default=datetime.now)
-  phone = models.CharField("numero de la aseguradora",max_length=50,default="",help_text="Indica el número para llamar en caso de un sinisetro")
+  price = models.FloatField("costo del seguro",default=0,help_text="costo el seguro")
+  cover_damage = models.FloatField("cobertura daños materiales",default=0)
+  cover_robbery = models.FloatField("cobertura robo",default=0)
+  cover_medic_expense = models.FloatField("cobertura medica",default=0)
+  cover_total = models.FloatField("cobertura total",default=0)
+  cover_type = models.CharField("tipo de cobertura",max_length=20,default= 0,choices = COVER_TYPE)
+  created_at = models.DateTimeField("fecha de alta",auto_now_add=True,editable=False)
+  last_modified = models.DateTimeField("ultima modificacion",auto_now=True,editable=False)
+  phone = models.CharField("numero de la aseguradora",max_length=10,default="",help_text="Indica el número para llamar en caso de un sinisetro, solo numeros")
   class Meta:
     verbose_name_plural = "Polizas"
 
   def __str__(self):
-    return self.company.name + ' (' + self.reference + ')'
+    status =""
+    if self.insurance_valid(): 
+      status = "Vigente"
+    else:
+      status = "Vencido"
+    return self.company.name + ' (' + self.reference + ')' + ' - ' + status
+
+  # Check if Insurance is valid
+  def insurance_valid(self):
+    if self.end_at > datetime.now(timezone.utc):
+      return True
+    else:
+      return False
+
+  #Validate end_at to be greater then start_at
+  def clean(self, *args, **kwargs):
+    if self.end_at < self.start_at:
+      raise ValidationError(_('Fin de vigencia debe ser mayor que Inicio de vigencia!'),code='invalid')
+    super(Policy, self).save(*args, **kwargs)
+
+
+
+
+
+
 
