2f7fefc1a78a65ded3c2e22a1d653921156bf69f
Juan Manuel Rivero <juanmanuelriverom@gmail.com>
2017-01-11 21:33:42 -0600

    fix driver model

--- drivers/models.py
+++ drivers/models.py
@@ -20,23 +20,18 @@ class Driver(models.Model):
     )
   BLOD_TYPE =(
     ("0","Seleccione el tipo de sangre"),
-    ("1","O-"),
-    ("2","O+"),
-    ("3","A-"),
-    ("4","A+"),
-    ("5","B-"),
-    ("6","B+"),
-    ("7","AB-"),
-    ("8","AB+"),
-    )
-  STATUS = (
-    ("0","N/A"),
-    ("1","activo"),
-    ("2","inactivo"),
+    ("O-","O-"),
+    ("O+","O+"),
+    ("A-","A-"),
+    ("A+","A+"),
+    ("B-","B-"),
+    ("B+","B+"),
+    ("AB-","AB-"),
+    ("AB+","AB+"),
     )
   GENDER = (
-    ("0","Masculino"),
-    ("1","Femanino"),
+    ("Masculino","Masculino"),
+    ("Femenino","Femanino"),
     )
   STATE = (
     ("0","Seleccione el estado"),
@@ -77,28 +72,31 @@ class Driver(models.Model):
   # General Driver data
   name = models.CharField("nombre/s",max_length=100)
   last_name = models.CharField("apellidos",max_length=100)
-  photo = models.ImageField("Foto chofer:",upload_to="drivers/",default="")
-  email = models.CharField("email",max_length=120,default="")
-  birthdate = models.DateTimeField("fecha de nacimiento",default=datetime.now)
-  blod_type = models.CharField("tipo de sangre",max_length=1,default= 0,choices = BLOD_TYPE)
+  photo = models.ImageField("foto chofer:",upload_to="drivers/",default="")
+  email = models.EmailField("email",max_length=254,default="")
+  phone = models.CharField("celular",max_length=10,default="")
+  birthdate = models.DateField("fecha de nacimiento",default=datetime.now)
+  gender = models.CharField("genero",max_length=9,default= 0,choices = GENDER)
+  blod_type = models.CharField("tipo de sangre",max_length=3,default= 0,choices = BLOD_TYPE,blank=True)
   # License data
-  license_type = models.CharField("tipo de licencia",max_length=1,default= 0,choices = LICENSE_TYPE)
-  license_number =  models.CharField("numero de licencia",max_length=100)
-  license_valid = models.DateTimeField("fecha de vencimiento",default=datetime.now)
+  license_type = models.CharField("tipo de licencia",max_length=1,default= 0,choices = LICENSE_TYPE,blank=True)
+  license_number =  models.CharField("numero de licencia",max_length=100,blank=True)
+  license_valid = models.DateTimeField("fecha de vencimiento",default=datetime.now,blank=True)
   # Official id data
-  ife_number = models.CharField("IFE",max_length=20,default=0)
-  curp = models.CharField("CURP",max_length=18,default=0)
+  ife_number = models.CharField("IFE",max_length=20,default=0,blank=True)
+  curp = models.CharField("CURP",max_length=18,default=0,blank=True)
   # Address data 
-  street = models.CharField("calle",max_length=20,default="")
-  ext_number = models.CharField("número exterior",max_length=20,default="")
-  int_number = models.CharField("numero interior",max_length=20,default=0)
-  zip_code = models.CharField("codigo postal",max_length=5,default="")
-  city = models.CharField("ciudad",max_length=20,default=0)
-  state = models.CharField("estado",max_length=2,default= 0,choices = STATE)
-  country = models.CharField("pais",max_length=20,default="México")
+  street = models.CharField("calle",max_length=150,default="",blank=True)
+  ext_number = models.CharField("número exterior",max_length=20,default="",blank=True)
+  int_number = models.CharField("número interior",max_length=20,default="",blank=True)
+  zip_code = models.CharField("codigo postal",max_length=5,default="",blank=True)
+  city = models.CharField("ciudad",max_length=20,default=0,blank=True)
+  state = models.CharField("estado",max_length=2,default= 0,choices = STATE,blank=True)
+  country = models.CharField("pais",max_length=20,default="México",blank=True)
   # Driver status
-  created_at = models.DateTimeField("fecha de alta",default=datetime.now)
-  status = models.CharField("status del chofer",max_length=1,default= 0,choices = STATUS)  
+  created_at = models.DateTimeField("fecha de alta",auto_now_add=True,editable=False)
+  last_modified = models.DateTimeField("ultima modificacion",auto_now=True,editable=False)
+  status = models.BooleanField("activo",default=True)  
   license_photo_front= models.ImageField("Foto licencia frente",upload_to="drivers/licence",default="",blank=True)
   license_photo_back = models.ImageField("Foto licencia reverso",upload_to="drivers/license",default="",blank=True)
   class Meta:
