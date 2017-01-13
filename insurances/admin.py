19001e50e729761e7f3eb8b3c00876f83324aa05
Juan Manuel Rivero <juanmanuelriverom@gmail.com>
2017-01-12 23:15:26 -0600

    clean code

--- insurances/admin.py
+++ insurances/admin.py
@@ -1,9 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
-from django.contrib import admin
-
 from .models import Company, Policy
+# Register your models here.
 
 admin.site.register(Company)
 admin.site.register(Policy)
