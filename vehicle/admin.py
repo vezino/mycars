da8b276b721cf1e1f21c11e93eda3d837fe2bcac
Juan Manuel Rivero <juanmanuelriverom@gmail.com>
2017-01-12 23:18:35 -0600

    add vehicle assignment model

--- vehicle/admin.py
+++ vehicle/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Vehicle,Brand
+from .models import Vehicle, Brand, VehicleAssigment
 admin.site.register(Brand)
 admin.site.register(Vehicle)
+admin.site.register(VehicleAssigment)
--- vehicle/models.py
+++ vehicle/models.py
@@ -5,6 +5,7 @@ from __future__ import unicode_literals
 from django.db import models
 from datetime import datetime
 from django.utils.encoding import python_2_unicode_compatible
+from drivers import models as models_drivers
 # Unicode 
 import unicodedata
 
@@ -59,9 +60,9 @@ class Vehicle(models.Model):
     verbose_name= "poliza")
   model_name = models.CharField("modelo",max_length=20)
   make_year = models.CharField("año de fabricacion",max_length=4,default= 0,choices = MODEL_YEAR)
-  plate = models.CharField("placa",max_length=10)
+  plate = models.CharField("placa",max_length=10,unique=True)
+  serial_number = models.CharField("numero de serie",max_length=30,unique=True,default=0)
   passengers = models.IntegerField('pasajeros',default=4)
-  uber_number = models.CharField("numero en UBER",max_length=20)
   color = models.CharField("color",max_length=20)
   created_at = models.DateTimeField("fecha de alta",auto_now_add=True,editable=False)
   last_modified = models.DateTimeField("ultima modificacion",auto_now=True,editable=False)
@@ -71,3 +72,24 @@ class Vehicle(models.Model):
     verbose_name_plural = "Vehiculos"
   def __str__(self):
     return self.brand.name + " " +self.model_name + "(" + self.plate + ")" 
+
+class VehicleAssigment(models.Model):
+  vehicle = models.ForeignKey(Vehicle,
+    on_delete=models.CASCADE,
+    related_name="Vehicle",
+    verbose_name= "vehiculo")
+  driver = models.ForeignKey(models_drivers.Driver,
+    on_delete=models.CASCADE,
+    related_name="vehicleassigment",
+    verbose_name="chofer")
+  start_at = models.DateTimeField("inicio de la asignacion",default=datetime.now)
+  initial_odo = models.FloatField("kilometraje inicial",default=0)
+  end_at = models.DateTimeField("fin de la asignacion",default=datetime.now)
+  final_odo = models.FloatField("kilometraje final",default=0)
+  created_at = models.DateTimeField("fecha de alta",auto_now_add=True,editable=False)
+  last_modified = models.DateTimeField("ultima modificacion",auto_now=True,editable=False)
+  class Meta:
+    verbose_name_plural = "Asignaciones de Vehiculo"
+  def __str__(self):
+    return self.vehicle.model_name + "( "+ self.vehicle.plate + ") - " + self.driver.name + " " +self.driver.last_name + " " + str(self.start_at)
+  
