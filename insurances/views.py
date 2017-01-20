6ac100f8d1979461ade0b81b40acc0a770cc6f60
Juan Manuel Rivero <juanmanuelriverom@gmail.com>
2017-01-19 21:46:13 -0600

    add login required

--- insurances/views.py
+++ insurances/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
-
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 from django.http import HttpResponse
 from django.views import View
 from django.shortcuts import render
 
+@login_required(login_url="/login/")
 def index(request):
     return HttpResponse("Hello, world. We are in development please go to admin to add catalog information.")
 
