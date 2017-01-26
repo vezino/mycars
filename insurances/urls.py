9578280908014b1e3211af3eed9444333b776fb6
Juan Manuel Rivero <juanmanuelriverom@gmail.com>
2017-01-25 23:55:02 -0600

    fix login required

--- insurances/urls.py
+++ insurances/urls.py
@@ -2,8 +2,9 @@ from django.conf.urls import url
 # Use login.
 from django.contrib.auth.decorators import login_required
 from . import views
+from insurances.views import LandingView
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^landing/$', views.LandingView.as_view(), name='land'),
+    url(r'^landing/$', login_required(LandingView.as_view(), login_url='/login/'), name="Landing"),
 ]
--- insurances/views.py
+++ insurances/views.py
@@ -3,14 +3,16 @@ from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 from django.http import HttpResponse
-from django.views import View
+#from django.views import View
 from django.shortcuts import render
+from django.views.generic import View, CreateView, ListView
 
 @login_required(login_url="/login/")
 def index(request):
     return HttpResponse("Hello, world. We are in development please go to admin to add catalog information.")
 
-class LandingView(View):
+#@login_required(login_url="/login/")
+class LandingView(CreateView):
   template_name = 'site_base.html'
   def get(self, request, *args, **kwargs):
     return render(request, self.template_name)
