2bce77e024ca495ed26d025e8afd9fa76edde540
Juan Manuel Rivero <juanmanuelriverom@gmail.com>
2017-01-25 22:42:46 -0600

    add expense data

--- expenses/models.py
+++ expenses/models.py
@@ -38,3 +38,47 @@ class Gasexpense(models.Model):
 
   def __str__(self):
     return str(self.date_load) + " - Asignacion: (" +  str(self.vehicleassigment) + ")" 
+
+@python_2_unicode_compatible
+class GasCardUsage(models.Model):
+  FUEL_TYPE = (
+    ("0","Selecciona el tipo de gasolina"),
+    ("Magna","Magna"),
+    ("Premium","Premium"),
+    ("Disel","Disel"),
+    )
+  company = models.CharField(max_length=50,default= "")
+  date_usage = models.DateField("fecha de carga",default=datetime.now)
+  time_usage = models.TimeField("hora de carga",default=datetime.now)
+  bin_number = models.CharField(max_length=20,default= "")
+  owner_name = models.CharField(max_length=100,default= "")
+  fuel_type = models.CharField("tipo de combustible",max_length=7,default= "",choices = FUEL_TYPE)
+  station_number = models.CharField(max_length=6,default= "")
+  plate = models.CharField(max_length=7,default= "")
+  kms = models.FloatField("kilometraje",default=0)
+  vehicle_description = models.CharField(max_length=100,default= "")
+  vehicle_serial_number = models.CharField(max_length=20,default= "")
+  charge_date = models.DateField("fecha de cargo",default=datetime.now)
+  gas_liters = models.FloatField("litros",default=0)
+  gas_price = models.FloatField("precio x litro",default=0)
+  ieps = models.FloatField("ieps",default=0)
+  initial_balance = models.FloatField("balance inicial",default=0)
+  final_balance = models.FloatField("balance final",default=0)
+  gas_total_without_tax = models.FloatField("total",default=0)
+  commission_without_tax = models.FloatField("comision sin iva",default=0)
+  comission_tax = models.FloatField("iva comision",default=0)
+  rendimiento = models.FloatField("rendimiento",default=0)
+  id_tx = models.IntegerField("id tx",default=0)
+  card_status = models.CharField("status de la tarjeta",max_length=20,default= "")
+  period = models.CharField("periodo",max_length=20,default= "")
+  invoice_folio = models.CharField("folio factura",max_length=20,default= "")
+  folio_tx = models.CharField("folio tx",max_length=20,default= "")
+  class Meta:
+    verbose_name_plural = "Tarjeta gastos gasolina"
+  def __str__(self):
+    return self.folio_tx + " " +str(self.date_usage) + "(" + self.plate + ")" 
+
+
+
+
+
