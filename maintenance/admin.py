a2a81f723da6cf7cef3cc12c55809db276772839
Juan Manuel Rivero <juanmanuelriverom@gmail.com>
2017-01-27 00:04:15 -0600

    clean code

--- maintenance/admin.py
+++ maintenance/admin.py
@@ -17,21 +17,8 @@ class ServiceOrderAdmin(admin.ModelAdmin):
   ]
   inlines = [PriceInline]
 
-# class ChoiceInline(admin.StackedInline):
-#     model = Choice
-#     extra = 3
 
-
-# class QuestionAdmin(admin.ModelAdmin):
-#     fieldsets = [
-#         (None,               {'fields': ['question_text']}),
-#         ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
-#     ]
-#     inlines = [ChoiceInline]
-
-#admin.site.register(Question, QuestionAdmin)
 admin.site.register(ServiceOrder,ServiceOrderAdmin)
 admin.site.register(ServiceConcept)
 admin.site.register(ServiceItem)  
 admin.site.register(ServiceItemPrice)  
-#admin.site.register(ServiceOrderPrice)  
