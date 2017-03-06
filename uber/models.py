dc94b7cb67babdb21ea664c8c236c369f16704eb
Juan Manuel Rivero <juanmanuelriverom@gmail.com>
2017-03-06 14:18:42 -0600

    add model for reports

--- uber/models.py
+++ uber/models.py
@@ -7,6 +7,8 @@ from django.utils.encoding import python_2_unicode_compatible
 # Unicode 
 import unicodedata
 # Create your models here.
+
+@python_2_unicode_compatible
 class UberData(models.Model):
   TRIP_TYPE = (
     ("Trip","Trip"),
@@ -35,3 +37,59 @@ class UberData(models.Model):
 
   def __str__(self):  
     return self.trip_number + " ( " + str(self.trip_date) + " )"
+
+
+@python_2_unicode_compatible
+class UberDataAgg(models.Model):
+  TRIP_TYPE = (
+  ("Trip","Trip"),
+  ("Misc Payment","Misc Payment")
+  )
+  driver_name = models.CharField(max_length=200,default= "")
+  phone_number = models.CharField(max_length=11,default="")
+  email = models.EmailField(max_length=254,default="")
+  trip_type = models.CharField(max_length=200,default= "",choices = TRIP_TYPE)  
+  trip_date = models.DateField(default=datetime.now,primary_key=True,unique=False)
+  trip_year = models.IntegerField(default=0,blank=True)
+  trip_month = models.IntegerField(default=0,blank=True)
+  trip_day = models.IntegerField(default=0,blank=True)
+  week = models.IntegerField(default=0,blank=True)
+  quarter = models.IntegerField(default=0,blank=True)
+  description = models.TextField(max_length=200,default= "",blank=True)
+  viajes = models.IntegerField(default=0)
+  fare = models.FloatField(default=0,blank=True)
+  surge = models.FloatField(default=0,blank=True)
+  toll = models.FloatField(default=0,blank=True)
+  misc = models.FloatField(default=0,blank=True)
+  other = models.FloatField(default=0,blank=True)
+  meter_rate = models.FloatField(default=0,blank=True)
+  gratuity = models.FloatField(default=0,blank=True)
+  commission = models.FloatField(default=0,blank=True)
+  tax_on_fee = models.FloatField(default=0,blank=True)
+  total_payment = models.FloatField(default=0,blank=True)
+  class Meta:
+    verbose_name_plural = "Aggregated Uber Data"
+    ordering = ['trip_date']
+    managed = False
+
+  def __str__(self):  
+    return self.driver_name + " ( " + str(self.trip_date) + " )"
+
+
+@python_2_unicode_compatible
+class VMycarsIncomeByYearMonth(models.Model):
+  trip_year = models.IntegerField(default=0,primary_key=True,unique=False)
+  trip_month = models.CharField(max_length=10,default=0)
+  viajes = models.IntegerField(default=0)
+  total_payment = models.FloatField(default=0)
+  class Meta:
+    verbose_name_plural = "Year Month Uber Data"
+    ordering = ['trip_year']
+    managed = False
+
+  def __str__(self):  
+    return str(self.trip_year) + " ( " + str(self.trip_month) + " )"  
+
+
+
+
