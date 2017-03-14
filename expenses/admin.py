7e9479df6b63f59c4ef7fa26abbc58046033ca48
Juan Manuel Rivero <juanmanuelriverom@gmail.com>
2017-03-13 23:33:32 -0600

    fix column display in admin

--- expenses/admin.py
+++ expenses/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+class GasexpenseAdmin(admin.ModelAdmin):
+  list_display = ('date_load','vehicleassigment')
+
 from .models import Gasexpense
-admin.site.register(Gasexpense)
+admin.site.register(Gasexpense,GasexpenseAdmin)
