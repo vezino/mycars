3a604a621f9efef419d0aaab703fa6818f149413
Juan Manuel Rivero <juanmanuelriverom@gmail.com>
2017-05-24 22:34:38 -0500

    add month year selector

--- reports/forms.py
+++ reports/forms.py
@@ -37,9 +37,34 @@ REPORT_YEARS = (
   (2016,"2016"),
   (2017,"2017"),
   (2018,"2018"),
+  (2019,"2019"),
+  (2020,"2020"),
   )
 
+REPORT_MONTHS = (
+  (1,"January"),
+  (2,"February"),
+  (3,"March"),
+  (4,"April"),
+  (5,"May"),
+  (6,"June"),
+  (7,"July"),
+  (8,"August"),
+  (9,"September"),
+  (10,"October"),
+  (11,"November"),
+  (12,"December"),    
+  )
 class YearSelector(forms.Form):
   #report_year = forms.DateField(widget=forms.SelectDateWidget(years=REPORT_YEARS))  
   report_year = forms.ChoiceField(widget=forms.Select, choices = REPORT_YEARS)
+
+class MonthYearSelector(forms.Form):
+  report_month = forms.ChoiceField(widget=forms.Select, choices = REPORT_MONTHS)
+  report_year = forms.ChoiceField(widget=forms.Select, choices = REPORT_YEARS)
+
+
+
+
+
 
